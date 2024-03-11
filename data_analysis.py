07fb0dfd528ddaae1f64d605906c95f7dad1992b
iamsaritas <146354979+iamsaritas@users.noreply.github.com>
2024-03-11 17:20:00 +0530

    Python file added

---
+++ data_analysis.py
@@ -0,0 +1,42 @@
+my_list = [1,2,3,4,5]
+print("Original list:",my_list)
+
+# Adding an element to the list
+my_list.append(6)
+print("List after adding an element:",my_list)
+
+# Removing an element from the list
+my_list.remove(3)
+print("List after removing an element:",my_list)
+
+# Modifying an element in the list
+my_list[0] = 10
+print("List after modifying an element:",my_list)
+
+# Creating a dictionary
+my_dict = {'a':1, 'b': 2, 'c': 3}
+print("Original dictionary:",my_dict)
+
+# Adding a key-value pair from the dictionary
+my_dict['d'] = 4
+print("Dictionary after adding key-value pair:",my_dict)
+
+# Removing a key-value pair from the dictionary
+del my_dict['b']
+print("Dictionary after removing key-value pari:",my_dict)
+
+#Modifying a value in the dictionary
+my_dict['a'] = 10
+print("Dictionary after midifying value: ",my_dict)
+
+#Creating a set
+my_set = {1,2,3,4,5}
+print("Original set:",my_set)
+
+#Adding an element to the set
+my_set.add(6)
+print("Set after adding element: ",my_set)
+
+#removing an element from the set
+my_set.remove(3)
+print("Set after removing alement: ",my_set)
